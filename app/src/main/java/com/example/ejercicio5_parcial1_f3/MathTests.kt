66b2c81b855e6d548c9b0371bb061cfd6f3f8f57
chuy-zip <andreschuy03@gmail.com>
2023-08-26 16:38:02 -0600

    Implemented E field of truncated cone

--- app/src/main/java/com/example/ejercicio5_parcial1_f3/MathTests.kt
+++ app/src/main/java/com/example/ejercicio5_parcial1_f3/MathTests.kt
@@ -3,22 +3,13 @@ package com.example.ejercicio5_parcial1_f3
 import kotlin.math.PI
 import kotlin.math.sqrt
 
+fun electricFieldOfDisc(qCharge: Double, distance: Double, radius: Double): Double {
+    val e = 8.85e-12
+    return (qCharge / (2 * PI * e * radius * radius)) * (1 - distance / sqrt(distance * distance + radius * radius))
+}
+
 fun heightFunctionOfSemicircle(maxRadius: Double, currentDistance: Double): Double {
     return sqrt(maxRadius * maxRadius - currentDistance * currentDistance)
-}
-
-//R1 is the base or bigger radius, R2 is the second or lower radius, H the height and x the point in which we want the radius "y"
-fun heightFunctionOfCutCone(R1:Double, R2: Double, H: Double, x: Double): Double{
-    return (R1 + ((R2-R1)/H) * x)
-}
-
-fun electricFieldOfDisc(QCharge: Double, distance: Double, radius: Double): Double {
-    val Eo = 8.85e-12
-
-    var sumOfElectricField = 0.0
-    sumOfElectricField  = (QCharge / (2 * PI * Eo * radius*radius) ) * (1 - distance/sqrt(distance*distance + radius*radius) )
-
-    return sumOfElectricField
 }
 fun volumeOfSemiCircle(radius: Double) {
     val qtyOfPartitions = 100
@@ -65,8 +56,8 @@ fun electricFieldOfHemisphere(charge: Double, pointDistance: Double, hemisphereR
 }
 
 //R is the radius of the cone, H the height and x the point in which we want the radius "y"
-fun heightFunctionOfCone(R:Double, H: Double, x: Double): Double{
-    return (R-(R/H)*x)
+fun heightFunctionOfCone(r:Double, h: Double, x: Double): Double{
+    return (r-(r/h)*x)
 }
 fun electricFieldOfCone(charge: Double, radius:Double, height: Double, pointDistance: Double): Double{
     val qtyOfPartitions = 1000
@@ -92,7 +83,12 @@ fun electricFieldOfCone(charge: Double, radius:Double, height: Double, pointDist
     return sumOfFields
 }
 
-fun electricFieldOfCutCone(charge: Double, radius1:Double, radius2:Double, height: Double, pointDistance: Double): Double{
+//R1 is the base or bigger radius, R2 is the second or lower radius, H the height and x the point in which we want the radius "y"
+fun heightFunctionOfTruncatedCone(r1:Double, r2: Double, h: Double, x: Double): Double{
+    return (r1 + ((r2-r1)/h) * x)
+}
+
+fun electricFieldOfTruncatedCone(charge: Double, radius1:Double, radius2:Double, height: Double, pointDistance: Double): Double{
     val qtyOfPartitions = 1000
     val partitionDistance = height / qtyOfPartitions
     val chargePerPartition = charge / qtyOfPartitions
@@ -101,10 +97,17 @@ fun electricFieldOfCutCone(charge: Double, radius1:Double, radius2:Double, heigh
     var sumOfFields = 0.0
 
     while(distance <= height){
-        val partitionRadius = heightFunctionOfCutCone(radius1, radius2, height, distance)
-        println("Height at $distance: $partitionRadius")
-        distance += partitionDistance
+        val partitionRadius = heightFunctionOfTruncatedCone(radius1, radius2, height, distance)
+        val distanceOfPartition = height - distance + pointDistance
+        val partitionField = electricFieldOfDisc(chargePerPartition, distanceOfPartition, partitionRadius)
+        //println("Field: $partitionField")
+        //println("Radius: $partitionRadius")
+        //println("distance: $distanceOfPartition")
+        //println("Height at $distance: $partitionRadius")
+        //println("--------")
 
+        sumOfFields += partitionField
+        distance += partitionDistance
     }
 
     return sumOfFields
@@ -116,7 +119,8 @@ fun main() {
 
     println("Hemisphere: " + electricFieldOfHemisphere(1e-6, 0.0001, 5.0))
 
-    println("Cone: " + electricFieldOfCone(1e-6,5.0,5.0,1.0))
-    //electricFieldOfCutCone(1e-6,10.0,5.0,5.0, 10.0)
+    println("Cone: " + electricFieldOfCone(1e-6,5.0,5.0,0.0001))
+
+    println("TruncatedCone: " + electricFieldOfTruncatedCone(1e-6,6.0,5.0,5.0, 0.0001))
 
 }
