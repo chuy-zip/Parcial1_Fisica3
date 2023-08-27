175b06a8098288caced109bee1faec308a8c9eaa
chuy-zip <andreschuy03@gmail.com>
2023-08-26 18:06:53 -0600

    Added xml files

---
+++ app/src/main/java/com/example/ejercicio5_parcial1_f3/MainActivityData.kt
@@ -0,0 +1,11 @@
+package com.example.ejercicio5_parcial1_f3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityData : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_data)
+    }
+}
